402400b03ac1fc7a09c42daa66249ca85f0e4b20
Elliott Pryor <pryorelliott@gmail.com>
2021-04-22 08:15:54 -0600

    Setup new test frame

--- src/shape_test.py
+++ src/shape_test.py
@@ -6,12 +6,33 @@ from src.approx import Approx
 from src.complete import Complete
 from src import point_readers, metrics, edge_selectors, dbscan
 from src.printer import file_printer
+from src import printer as printer_class
 from src.dbscan import dbscanner
 import sklearn.metrics as sk
 import sys
 
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d as plt3
+
+
+def run_complete(file, path=""):
+    s = time.time()
+    c = Complete(file, point_readers.shape_r, metrics.hausdorff_cloud)
+    file_printer("Complete: " + str(time.time() - s))
+    if path == "":
+        c.mtx_to_file(path="../distances/shapes-500")
+    else:
+        c.mtx_to_file(path=path)
+
+
+def run_approx(file, eps, path=""):
+    s = time.time()
+    c = Approx(file, eps, point_readers.shape_r, metrics.hausdorff_cloud)
+    file_printer("Approx Eps {:f}: {:f}".format(eps,time.time() - s))
+    if path == "":
+        c.mtx_to_file(path="../distances/shapes-500")
+    else:
+        c.mtx_to_file(path=path)
 
 
 if __name__ == "__main__":
@@ -22,20 +43,21 @@ if __name__ == "__main__":
 
     file = '../gen_shapes/'
 
-    s = time.time()
-    c = Complete(file, point_readers.shape_r, metrics.hausdorff_cloud)
-    file_printer("Complete: " + str(time.time() - s))
-    c.mtx_to_file(path="../distances/shapes-500")
+    eps = [0.5, 1, 1.5, 2, 5]
+    printer_class.PRIORITY = 1
+    for e in eps:
+
+        run_approx(file, e)
+
+
+    run_complete(file)
     # db = dbscanner(c.matrix, 1)
     # db.run()
     # # db.plot(c.points, title='Actual')
     # db.plot(c.matrix, title='Actual')
     print()
 
-    s = time.time()
-    a = Approx(file, 3, point_readers.shape_r, metrics.hausdorff_cloud, edge_selectors.blind_greedy)
-    file_printer("Approx (eps=3): " + str(time.time() - s))
-    a.mtx_to_file(path="../distances/shapes-500")
+    run_approx(file, 3)
     # db2 = dbscanner(a.matrix, 1)
     # db2.run()
     # db2.plot(a.matrix, title="Epsilon = 3")
